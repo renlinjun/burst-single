0f207b26ff8d679aef3e93d8bfe3794c440d28c4
iikspiral <738132704@qq.com>
2018-05-31 16:12:11 +0800

    测试提交test01

--- src/main/java/burst/App.java
+++ src/main/java/burst/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class App {
-
+    //*-*-*-*-*-*
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
